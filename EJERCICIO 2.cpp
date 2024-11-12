54899c5d41721a28b514ecb8b2ec41bc9bcc52a5
joao20D <djgallo@espe.edu.ec>
2024-11-11 21:19:20 -0500

    Add files via upload

---
+++ EJERCICIO 2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+float p1,p2,p3,pt;
+cout<<"INGRESAR NOTA PACIAL 1:"<<endl;
+cin>>p1;
+cout<<"INGRESAR NOTA PARCIAL 2:"<<endl;
+cin>>p2;
+cout<<"INGRESAR NOTA PARCIAL 3:"<<endl;
+cin>>p3;
+pt=p1+p2+p3;
+cout<<"EL PROMEDIO ES:"<<pt<<endl;
+
+if(pt<42) {
+
+    cout<<"no pasa"<<endl;
+
+
+}
+else{
+cout<<"si pasa"<<endl;
+}
+}
